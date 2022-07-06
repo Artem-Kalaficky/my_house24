c953510661dd06023b88fb2a6616bd10939aa912
Artem <kalaficzkij@ukr.net>
2022-07-06 22:22:03 +0300

    last version of the project (develop users pages)

---
+++ crm/static/crm/scripts/users/create_user.js
@@ -0,0 +1,36 @@
+// generate and show passwords
+$(document).ready(function() {
+	function str_rand() {
+		var result       = '';
+		var words        = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+		var max_position = words.length - 1;
+			for( i = 0; i < 10; ++i ) {
+				position = Math.floor ( Math.random() * max_position );
+				result = result + words.substring(position, position + 1);
+			}
+		return result;
+	}
+	$('.showPassword').click(function(){
+		var inputPsw = $('#id_password1');
+		if (inputPsw.attr('type') == 'password') {
+			document.getElementById('id_password1').setAttribute('type', 'text');
+			document.getElementById('id_password2').setAttribute('type', 'text');
+		} else {
+			document.getElementById('id_password1').setAttribute('type', 'password');
+			document.getElementById('id_password2').setAttribute('type', 'password');
+		}
+	});
+	$('.generatePassword').click(function() {
+		document.getElementById('id_password1').setAttribute('type', 'text');
+		$('#id_password1').attr('value', str_rand());
+		$('#id_password2').attr('value', $('#id_password1').val())
+		document.getElementById('id_password1').setAttribute('type', 'password');
+		document.getElementById('id_password2').setAttribute('type', 'password');
+	});
+});
+
+
+// Mask for telephone
+$(function(){
+  $("#id_telephone").mask("+380(099) 999-99-99");
+});
